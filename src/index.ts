86e364f74e030db9d9eda9b7ab21ecd24d7b8242
wangjiaw <wangjiaw@chanjet.com>
2021-01-17 13:07:00 +0800

    泛型T的广泛利用

--- src/index.ts
+++ src/index.ts
@@ -12,40 +12,67 @@
 // logDetails(1, '1')
 // greet({name: 'claus', uid: '1'})
 
-let greet: (name: string, age: number) => string |number
+// let greet: (name: string, age: number) => string |number
+//
+// greet = (name: string, age: number ): string |number => {
+//     console.log(`${name} say my age is ${age}`)
+//     return 1
+// }
+//
+// greet('claus', 12)
+//
+//
+// let calc: (numOne: number, numTwo: number, action: string) => number
+//
+// calc = (numOne: number, numTwo: number, action: string): number => {
+//     if (action === 'add') {
+//         return numOne + numTwo
+//     } else {
+//         return numOne - numTwo
+//     }
+// }
+//
+// console.log(calc(1, 2, 'add'))
+//
+//
+// type person = {
+//     name: string,
+//     age: number
+// }
+//
+// let logDetails : (obj: person) => void
+//
+// logDetails = (obj: person): void => {
+//     console.log(`${obj.name} ${obj.age}`)
+// }
 
-greet = (name: string, age: number ): string |number => {
-    console.log(`${name} say my age is ${age}`)
-    return 1
+// 泛型 T 应用
+// let saySomething = (obj: object) => {
+// let saySomething = <T>(obj: T) => {
+let saySomething = <T extends { love: string }>(obj: T) => {
+    return {...obj}
 }
 
-greet('claus', 12)
+let sayOne = saySomething({ love: 'you', eat: 'soul' })
 
+interface Letter<T> {
+    title: string;
+    content: string;
+    others: T
+}
 
-let calc: (numOne: number, numTwo: number, action: string) => number
-
-calc = (numOne: number, numTwo: number, action: string): number => {
-    if (action === 'add') {
-        return numOne + numTwo
-    } else {
-        return numOne - numTwo
+const girlFriendLetter: Letter<object> = {
+    title: 'I love you',
+    content: 'forever',
+    others: {
+        msg: 'wait for you night'
     }
 }
-
-console.log(calc(1, 2, 'add'))
-
-
-type person = {
-    name: string,
-    age: number
+const ex_girlFriendeLetter: Letter<string> = {
+    title: 'I hate you',
+    content: 'ever',
+    others: 'kick you ass'
 }
-
-let logDetails : (obj: person) => void
-
-logDetails = (obj: person): void => {
-    console.log(`${obj.name} ${obj.age}`)
-}
-
 
 
 
