fca1fa057c1e04bd5bcf6ab7d35a38d124480d4e
wangjiaw <wangjiaw@chanjet.com>
2021-01-17 10:51:04 +0800

    # This is a combination of 3 commits. # This is the 1st commit message:
    class类类型的利用，省去constructor初始化
    
    # This is the commit message #2:
    
    class类类型的利用
    1. public、private、readonly
    2. constructor传值简略写法
    
    # This is the commit message #3:
    
    class类类型的利用,

--- dist/indexClass.js
+++ dist/indexClass.js
@@ -3,17 +3,21 @@ class Invoice {
         this.client = client;
         this.details = details;
         this.amount = amount;
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} owes ￥${this.amount}k for ${this.details}`;
+    }
+    a() {
+        return this.client = '1';
     }
 }
 const invOne = new Invoice('Claus', 'working for the Chanjet', 25);
 const invTwo = new Invoice('Summer', 'working for the 58', 16);
 console.log(invOne);
 console.log(invTwo);
-console.log(invOne.client);
-invOne.client = `2092's Me`;
 console.log(invOne.format());
 console.log(invTwo.format());
 let invoices = [];
