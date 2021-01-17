b0c5e50611e43543052a5955f526debdfd5bc8e8
wangjiaw <wangjiaw@chanjet.com>
2021-01-17 11:08:44 +0800

    class类类型的利用 1. public、private、readonly 2. constructor传值简略写法

--- src/indexClass.ts
+++ src/indexClass.ts
@@ -1,30 +1,39 @@
 class Invoice {
-    client: string
-    details: string
-    amount: number
+    // private client: string
+    // readonly details: string
+    // public amount: number
 
-    // constructor(client: string, details: string, amount: number) {
-    //     this.client = client
-    //     this.details = details
-    //     this.amount = amount
-    // }
+    constructor(
+        private client: string, // 类型限定 简写
+        readonly details: string,
+        public amount: number
+    ) {
+        this.client = client
+        this.details = details
+        this.amount = amount
+    }
 
     format() {
         return `${this.client} owes ￥${this.amount}k for ${this.details}`
     }
+
+    a() {
+        return this.client = '1'
+    }
 }
 
-// const invOne = new Invoice('Claus', 'working for the Chanjet', 25)
-// const invTwo = new Invoice('Summer', 'working for the 58', 16)
-const invOne = new Invoice()
-const invTwo = new Invoice()
+/**
+ * public、private、readonly
+ * 1. public 正常无
+ * 2. private 在类内部玩
+ * 3. readonly 内、外都只能读取，不可修改
+ */
+
+const invOne = new Invoice('Claus', 'working for the Chanjet', 25)
+const invTwo = new Invoice('Summer', 'working for the 58', 16)
 
 console.log(invOne)
 console.log(invTwo)
-
-invOne.client = `2092's Me`
-invOne.details = `working for the Chanjet`
-invOne.amount = 30
 
 console.log(invOne.format())
 console.log(invTwo.format())
