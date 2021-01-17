e6d52f45dc8302b0da34799e6e8aa171922365b7
wangjiaw <wangjiaw@chanjet.com>
2021-01-17 12:52:46 +0800

    非空断言，欺骗编辑器

--- dist/indexClass.js
+++ dist/indexClass.js
@@ -2,8 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Invoice_1 = require("./classes/Invoice");
 const Payment_1 = require("./classes/Payment");
-const invOne = new Invoice_1.Invoice('Claus', 'working for the Chanjet', 25);
-const invTwo = new Payment_1.Payment('Summer', 'shopping in the Walmart', 100);
-console.log(invOne.amount);
+let invOne;
+let invTwo;
+invOne = new Invoice_1.Invoice('Claus', 'working for the Chanjet', 25);
+invTwo = new Payment_1.Payment('Summer', 'shopping in the Walmart', 100);
+console.log(invOne.format());
 console.log(invTwo.format());
+console.log(invTwo.name.trim());
 //# sourceMappingURL=indexClass.js.map
