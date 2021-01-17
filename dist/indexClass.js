a0cac87b3bed9e30c57aeb4a2991000333a0090e
wangjiaw <wangjiaw@chanjet.com>
2021-01-17 11:08:44 +0800

    class类类型的利用 1. public、private、readonly 2. constructor传值简略写法

--- dist/indexClass.js
+++ dist/indexClass.js
@@ -1,15 +1,23 @@
 class Invoice {
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
     format() {
         return `${this.client} owes ￥${this.amount}k for ${this.details}`;
     }
+    a() {
+        return this.client = '1';
+    }
 }
-const invOne = new Invoice();
-const invTwo = new Invoice();
+const invOne = new Invoice('Claus', 'working for the Chanjet', 25);
+const invTwo = new Invoice('Summer', 'working for the 58', 16);
 console.log(invOne);
 console.log(invTwo);
-invOne.client = `2092's Me`;
-invOne.details = `working for the Chanjet`;
-invOne.amount = 30;
 console.log(invOne.format());
 console.log(invTwo.format());
 let invoices = [];
