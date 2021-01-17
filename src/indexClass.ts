c229788c04efb15384eb8596889998180769948b
wangjiaw <wangjiaw@chanjet.com>
2021-01-17 12:52:46 +0800

    非空断言，欺骗编辑器

--- src/indexClass.ts
+++ src/indexClass.ts
@@ -1,11 +1,17 @@
 import { Invoice } from "./classes/Invoice";
 import { Payment } from "./classes/Payment";
+import { HasFormatter } from "./interfaces/HasFormatter";
 
-const invOne = new Invoice('Claus', 'working for the Chanjet', 25)
-const invTwo = new Payment('Summer', 'shopping in the Walmart', 100)
+let invOne: HasFormatter
+let invTwo: HasFormatter
 
-console.log(invOne.amount)
+invOne = new Invoice('Claus', 'working for the Chanjet', 25)
+invTwo = new Payment('Summer', 'shopping in the Walmart', 100)
+
+console.log(invOne.format())
 console.log(invTwo.format())
+
+console.log(invTwo.name!.trim()) // 非空断言，欺骗编辑器为非空
 
 
 
